bea10499f8b49031e32a09c5fe34af7916c416b9
BajiLinux <mr.baji.smart@gmail.com>
2026-06-06 01:05:37 +0700

    day-6: ngantuk, nanti dilanjut lagi

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+import { eventNames, listeners } from "process";
 import Bookshelf from "./day-4/bookData.js";
+import { EventEmitter } from "events";
+
+const myEmitter = new EventEmitter();
+
+const bookAddedListener = (bookName) => {
+  console.log(`Notifikasi Industri: Buku ${bookName} berhasil di simpan di Bookshelf`);
+};
+
+on(eventNames, listeners)
+
+.emit(eventNames, data);
